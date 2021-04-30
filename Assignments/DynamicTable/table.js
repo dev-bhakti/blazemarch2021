fba961e24b3ad095b21bd0a853f0814e05f05943
Bhakti Thombare <bhakti.thombare@blazeclan.com>
2021-04-30 22:49:58 +0530

    changes

--- Assignments/DynamicTable/table.js
+++ Assignments/DynamicTable/table.js
@@ -1,90 +1,105 @@
-class tableLogic{
-    productList=[];   
-    constructor(){
-        this.obj = new UIGenerator()
+class tableLogic {
+    productList = [];
+    constructor() {
+        this.uiObj = new TableUIGenerator()
         this.PageSize = 5
         this.Delete = true
         this.Edit = true
-        this.CheckBox= true
+        this.CheckBox = true
         this.productList = [
-                            {Sr_No :1,Product_Id:'Prd-1', Product_Name:'Laptop', Category:'Electronics', Manufacturer:"IBM", Description:"Description1",Base_Price:40000},
-                            {Sr_No :2,Product_Id:'Prd-2', Product_Name:'Mouse', Category:'Electrical', Manufacturer:"TATA", Description:"Description2",Base_Price:2500},
-                            {Sr_No :3,Product_Id:'Prd-3', Product_Name:'Cadbury', Category:'Food', Manufacturer:"PARLE", Description:"Description3",Base_Price:1000},
-                            {Sr_No :4,Product_Id:'Prd-4', Product_Name:'Mobile', Category:'Electronics', Manufacturer:"TATA", Description:"Description4",Base_Price:20000},
-                            {Sr_No :5,Product_Id:'Prd-5', Product_Name:'Keyboard', Category:'Electrical', Manufacturer:"IBM", Description:"Description5",Base_Price:3000},
-                            {Sr_No :6,Product_Id:'Prd-6', Product_Name:'Chair', Category:'Furniture', Manufacturer:"BAJAJ", Description:"Description6",Base_Price:12000},
-                            {Sr_No :7,Product_Id:'Prd-7', Product_Name:'Laptop', Category:'Electronics', Manufacturer:"TATA", Description:"Description7",Base_Price:60000},
-                            
-                            {Sr_No :8,Product_Id:'Prd-8', Product_Name:'Charger', Category:'Electronics', Manufacturer:"IBM", Description:"Description8",Base_Price:500},
-                            {Sr_No :9,Product_Id:'Prd-9', Product_Name:'T.V', Category:'Electrical', Manufacturer:"RELIANCE", Description:"Description9",Base_Price:25000},
-                            {Sr_No :10,Product_Id:'Prd-10', Product_Name:'Chocolate', Category:'Food', Manufacturer:"NESTLE", Description:"Description10",Base_Price:2000},
-                            {Sr_No :11,Product_Id:'Prd-11', Product_Name:'Straightner', Category:'Electronics', Manufacturer:"BAJAJ", Description:"Description11",Base_Price:999},
-                            {Sr_No :12,Product_Id:'Prd-12', Product_Name:'Keyboard', Category:'Electrical', Manufacturer:"IBM", Description:"Description12",Base_Price:3200},
-                            {Sr_No :13,Product_Id:'Prd-13', Product_Name:'Table', Category:'Furniture', Manufacturer:"BAJAJ", Description:"Description13",Base_Price:13000},
-                            ];
-                
-                            this.state= {
-                                'data': this.productList,
-                                'state': 1,
-                                'rows': this.PageSize  
-        
-                            }
-                            
-    }
-    
-    
-    //To show the pagination number
-    pagination=(prod,page,rows)=>{
-        this.trimStart = (page-1) * rows
-        this.trimEnd = this.trimStart + rows
-         this.trimdata = prod.slice(this.trimStart,this.trimEnd)
-        this.pageNo = Math.round(prod.length / rows);
-        return{
-            'prod':this.trimdata,
-            'pageNo':this.pageNo
+            { Sr_No: 1, Product_Id: 'Prd-1', Product_Name: 'Laptop', Category: 'Electronics', Manufacturer: "IBM", Description: "Description1", Base_Price: 40000 },
+            { Sr_No: 2, Product_Id: 'Prd-2', Product_Name: 'Mouse', Category: 'Electrical', Manufacturer: "TATA", Description: "Description2", Base_Price: 2500 },
+            { Sr_No: 3, Product_Id: 'Prd-3', Product_Name: 'Cadbury', Category: 'Food', Manufacturer: "PARLE", Description: "Description3", Base_Price: 1000 },
+            { Sr_No: 4, Product_Id: 'Prd-4', Product_Name: 'Mobile', Category: 'Electronics', Manufacturer: "TATA", Description: "Description4", Base_Price: 20000 },
+            { Sr_No: 5, Product_Id: 'Prd-5', Product_Name: 'Keyboard', Category: 'Electrical', Manufacturer: "IBM", Description: "Description5", Base_Price: 3000 },
+            { Sr_No: 6, Product_Id: 'Prd-6', Product_Name: 'Chair', Category: 'Furniture', Manufacturer: "BAJAJ", Description: "Description6", Base_Price: 12000 },
+            { Sr_No: 7, Product_Id: 'Prd-7', Product_Name: 'Laptop', Category: 'Electronics', Manufacturer: "TATA", Description: "Description7", Base_Price: 60000 },
+            { Sr_No: 8, Product_Id: 'Prd-8', Product_Name: 'Charger', Category: 'Electronics', Manufacturer: "IBM", Description: "Description8", Base_Price: 500 },
+            { Sr_No: 9, Product_Id: 'Prd-9', Product_Name: 'T.V', Category: 'Electrical', Manufacturer: "RELIANCE", Description: "Description9", Base_Price: 25000 },
+            { Sr_No: 10, Product_Id: 'Prd-10', Product_Name: 'Chocolate', Category: 'Food', Manufacturer: "NESTLE", Description: "Description10", Base_Price: 2000 },
+            { Sr_No: 11, Product_Id: 'Prd-11', Product_Name: 'Straightner', Category: 'Electronics', Manufacturer: "BAJAJ", Description: "Description11", Base_Price: 999 },
+            { Sr_No: 12, Product_Id: 'Prd-12', Product_Name: 'Keyboard', Category: 'Electrical', Manufacturer: "IBM", Description: "Description12", Base_Price: 3200 },
+            { Sr_No: 13, Product_Id: 'Prd-13', Product_Name: 'Table', Category: 'Furniture', Manufacturer: "BAJAJ", Description: "Description13", Base_Price: 13000 },
+            { Sr_No: 14, Product_Id: 'Prd-14', Product_Name: 'Headphone', Category: 'Electronics', Manufacturer: "JBL", Description: "Description14", Base_Price: 900 },
+            { Sr_No: 15, Product_Id: 'Prd-15', Product_Name: 'LED', Category: 'Electrical', Manufacturer: "MI", Description: "Description15", Base_Price: 25000 },
+            { Sr_No: 16, Product_Id: 'Prd-16', Product_Name: 'Chocolate', Category: 'Food', Manufacturer: "NESTLE", Description: "Description16", Base_Price: 1000 },
+            { Sr_No: 17, Product_Id: 'Prd-17', Product_Name: 'Mouse', Category: 'Electronics', Manufacturer: "LENOVO", Description: "Description17", Base_Price: 1999 },
+            { Sr_No: 18, Product_Id: 'Prd-18', Product_Name: 'Keyboard', Category: 'Electrical', Manufacturer: "HP", Description: "Description18", Base_Price: 3000 },
+            { Sr_No: 19, Product_Id: 'Prd-19', Product_Name: 'Table', Category: 'Furniture', Manufacturer: "BAJAJ", Description: "Description19", Base_Price: 3000 },
+            { Sr_No: 20, Product_Id: 'Prd-20', Product_Name: 'Chair', Category: 'Furniture', Manufacturer: "PLY", Description: "Description20", Base_Price: 1300 },
+        ];
+
+        this.state = {
+            'state': 1,
+            'rows': this.PageSize,
+            'data': this.productList,
         }
     }
-
-    
-//To display pagination buttons
-    PaginationButtons =(pages)=>{
-        
-        var pagination = document.getElementById('pagination')
-        for( var pg=1; pg<=pages ;pg++){
-            pagination.innerHTML+=`<button value=${pg} class='page btn-sm btn btn-warning' style="border:2px solid grey;" name='page'>${pg}</button>`
-        }
-
-       var page_btn= document.getElementsByName('page')
-      
-        for(var i of page_btn){
-            i.addEventListener('click',this.pageButton,false)
-
-        }
-        
-    }
-    //when clicked on  pageButton, will go to the respective page
-    pageButton=(e)=>{
-                document.getElementById('myTable').innerHTML=''
-                document.getElementById('pagination').innerHTML=''
-                
-        this.state['state']=e.target.value
-        this.getTableData()
-        
-    }
-
     //get table with headers,columns and buttons
-    getTableData=()=>{
-        this.productList=this.pagination(this.state['data'],this.state['state'],this.state['rows'])
-  
-        document.getElementById('myTable').innerHTML=this.obj.DisplayTable(this.productList['prod'],this.Delete,this.Edit)       
+    getTableData = () => {
+        document.getElementById('myTable').innerHTML = ''
+        this.productList = this.pagination(this.state['data'], this.state['state'], this.state['rows'])
+        document.getElementById('myTable').innerHTML = this.uiObj.DisplayTable(this.productList['prod'], this.Delete, this.Edit)
+        document.getElementById('pagination').innerHTML = ''
         this.PaginationButtons(this.productList['pageNo'])
-        
-    }    
+
+    }
+
+    //To show the pagination number
+    pagination = (prod, page, rows) => {
+        this.trimStart = (page - 1) * rows
+        this.trimEnd = parseInt(this.trimStart) + parseInt(rows)
+        this.trimdata = prod.slice(this.trimStart, this.trimEnd)
+        this.pageNo = Math.round(prod.length / rows);
+        return {
+            'prod': this.trimdata,
+            'pageNo': this.pageNo
+        }
+    }
+
+    //To display pagination buttons
+    PaginationButtons = (pages) => {
+
+        var pagination = document.getElementById('pagination')
+        for (let p = 1; p <= pages; p++) {
+            pagination.innerHTML += `<button value=${p} name='pagebtn' class='btn btn-warning' style="border:2px solid grey;" >${p}</button>`
+        }
+        let btn = document.getElementsByName('pagebtn')
+
+        for (var b of btn) {
+            b.addEventListener('click', this.pageButtonClick, false)
+
+        }
+
+    }
+
+    //when clicked on  pageButton, will go to the respective page
+    pageButtonClick = (e) => {
+        document.getElementById('myTable').innerHTML = ''
+        document.getElementById('pagination').innerHTML = ''
+
+        this.state['state'] = e.target.value
+        this.getTableData()
+
+    }
+
+    displayRows = (rows) => {
+        this.state['rows'] = rows
+        this.getTableData()
+    }
+
 }
+
 
 //to show table with the logic of pagination and table
-function showTable(){
+function showTable() {
     prd = new tableLogic
     prd.getTableData()
+
+    getPageSize = (rows) => {
+        prd.displayRows(rows)
+    }
+
 }
+
 
--- Assignments/DynamicTable/uigenerate_table.js
+++ Assignments/DynamicTable/uigenerate_table.js
@@ -1,4 +1,4 @@
-class UIGenerator {
+class TableUIGenerator {
     constructor(){}
 
      DisplayTable(data,Delete_Row,Edit_Row){
@@ -18,8 +18,9 @@ class UIGenerator {
         table+=`<th><input class="checkbox" style="margin-left:32%; margin-top:23%;" type="checkbox" id="select_all" onclick="selectAllCheckboxes(this)"><br>Select All </th>`
       
         for(var head = 0; head < headers.length; head++){
-            // table+= `<th onclick= 'sortTable(0)'>${headers[head]}</th>`; 
-            table+= "<th>"+headers[head]+ "<br><span style='cursor: pointer;' onclick= 'sortTable(1)'>&#9650 </span>" +"</th>";
+          table+= `<th >${headers[head]}</th>`; 
+            //  table+= `<th onclick= 'sortTable(0)'>${headers[head]}</th>`; 
+            // table+= "<th>"+headers[head]+ "<br><span style='cursor: pointer;' onclick= 'sortTable(0)'>&#9650 </span>" +"</th>";
         } 
 
         if( Edit_Row == true && Delete_Row == true){
@@ -27,16 +28,15 @@ class UIGenerator {
         }
         table+=`</tr></thead>`;
         table+=`<tbody>`;
-        for(let row=0;row<data.length;row++){
+        for(let row = 0; row < data.length; row++){
             
             table+=`<tr  id=${row} >`;
             table+=`<td id='check'><input class="checkbox" style="margin-left:32%;margin-top:10%;" type="checkbox" name="check"  id="${row}"></td>`
-            
-            table+= `<td value=${data[row][headers[0]]}  >${data[row][headers[0]]}</td>`
-            for(var val=1;val<headers.length;val++){
+
+            for(var val = 1 ; val<headers.length;val++){
                 //value in input is for showing the already presented values
-                table+= `<td>
-                <span id='span-${row}-${val}'>${data[row][headers[val]]}</span> 
+                table+= `<td value =${data[row][headers[val]]}>
+                <span id='sp-${row}-${val}'>${data[row][headers[val]]}</span> 
                 <input type='text' value=${data[row][headers[val]]} id='${row}-${val}' style='display:none'> 
                 
                 </td>`; 
@@ -45,21 +45,18 @@ class UIGenerator {
 
             let prodData=JSON.stringify(data) // to show the products data in table
             if(Edit_Row== true && Delete_Row==true) //To show edit and delete buttons in column
-            table+= `<td id='rowid${row}-${val}'><button class="btn btn-success" id='E-${row}-${val}' onclick=EditRow(${prodData},'${row}','${val}')>Edit</button>
+            table+= `<td id='row${row}-${val}'><button class="btn btn-success" id='E-${row}${val}' onclick=EditRow(${prodData},'${row}','${val}')>Edit</button>
             </td>`
-            table+= `<td><button class="btn btn-danger" id='${row}' onclick='DeleteRow(${row},${prodData})'>Delete</button>
+            table+= `<td><button class="btn btn-danger" id='${row}${val}' onclick='DeleteRow(${row},${prodData})'>Delete</button>
             </td>`
             table+=`</tr>`;           
         }
         
         table+="</tbody>";
         table+=`<tfoot id='pages'><tr></tr></tfoot>`;
-        table+="</table>";
-        
-        
-        
+        table+="</table>";        
         return table;
-        //return table;
+
 
     }
         
@@ -81,59 +78,59 @@ function selectAllCheckboxes(e)
 }
 
 
-function sortTable(n) {
-    var table, rows, sort,sort1, i, a1, a2, order, count = 0;
-    table = document.getElementById("myTable");
-    sort = true;
-    order = "asc"; 
-    while (sort) {
-      sort = false;
-      rows = table.rows;
-      for (i = 1; i < (table.rows - 1); i++) {
-        sort1 = false;
-        a1 = rows[i].getElementsByTagName("td")[n];
-        a2 = rows[i + 1].getElementsByTagName("td")[n];
+// function sortTable(n) {
+//     var table, rows, sort,sort1, i, a1, a2, order, count = 0;
+//     table = document.getElementById("myTable");
+//     sort = true;
+//     order = "asc"; 
+//     while (sort) {
+//       sort = false;
+//       rows = table.rows;
+//       for (i = 1; i < (table.rows - 1); i++) {
+//         sort1 = false;
+//         a1 = rows[i].getElementsByTagName("td")[n];
+//         a2 = rows[i + 1].getElementsByTagName("td")[n];
   
-        if (order == "asc") {
-          if (a1.innerHTML.toLowerCase() > a2.innerHTML.toLowerCase()) {
-            sort1= true;
-            break;
-          }
-        } else if (order == "desc") {
-          if (a1.innerHTML.toLowerCase() < a2.innerHTML.toLowerCase()) {
+//         if (order == "asc") {
+//           if (a1.innerHTML.toLowerCase() > a2.innerHTML.toLowerCase()) {
+//             sort1= true;
+//             break;
+//           }
+//         } else if (order == "desc") {
+//           if (a1.innerHTML.toLowerCase() < a2.innerHTML.toLowerCase()) {
             
-            sort1 = true;
-            break;
-          }
-        }
-      }
-      if (sort1) {
+//             sort1 = true;
+//             break;
+//           }
+//         }
+//       }
+//       if (sort1) {
         
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        sort = true;
-        count ++;      
-      } else {
-        if (count == 0 && order == "asc") {
-          order = "desc";
-          sort = true;
-        }
-      }
-    }
-  }
+//         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//         sort = true;
+//         count ++;      
+//       } else {
+//         if (count == 0 && order == "asc") {
+//           order = "desc";
+//           sort = true;
+//         }
+//       }
+//     }
+//   }
 
 //function  to edit the row 
 function EditRow(data,row1,row2){
  
-edit=`rowid${row1}-${row2}`
+edit=`row${row1}-${row2}`
 for( let i=1;i<row2;i++){
     input=`${row1}-${i}`
-    span_id=`span-${row1}-${i}`
+    span=`sp-${row1}-${i}`
 
-    document.getElementById(span_id).style.display='none'
+    document.getElementById(span).style.display='none'
     document.getElementById(input).style.display='block'
 
     document.getElementById(edit).innerHTML=
-    `<button class="btn btn-info" onclick='Edit(${JSON.stringify(data)},${row1},${row2})'>Save</button>
+    `<button class="btn btn-info" onclick='Edit(${JSON.stringify(data)},${row1},${row2})'>Update</button>
     <button class="btn btn-warning"  style="margin-top:5px" onclick=getRow(${row1},${row2})>Cancel</button>`
     }
 }
@@ -142,11 +139,11 @@ for( let i=1;i<row2;i++){
 function getRow(row,col)
 {
     for(i=1;i<col;i++){
-        span_id=`span-${row}-${i}`
+        span=`sp-${row}-${i}`
         input=`${row}-${i}`
-        edit=`rowid${row}-${col}`
+        edit=`row${row}-${col}`
         
-        document.getElementById(span_id).style.display='block'
+        document.getElementById(span).style.display='block'
         document.getElementById(input).style.display='none'
         document.getElementById(edit).innerHTML=
         `<button class="btn btn-success" id='E-${row}-${col}' onclick=EditRow('${row}','${col}')>Edit</button>`
@@ -160,26 +157,25 @@ function getRow(row,col)
 function Edit(data,row,col){
     let headers = Object.keys(data[0]);
     tbl_row_id=`Row-0${row}`
-    for(let c=1;c<headers.length;c++){
-        span_id=`span-${row}-${c}`
-        input=`${row}-${c}`
+    for(let e = 1; e < headers.length; e++){
+        span=`sp-${row}-${e}`
+        input=`${row}-${e}`
         let i=`${row}`
-        data[i][headers[c]]=document.getElementById(input).value
+        data[i][headers[e]]=document.getElementById(input).value
         document.getElementById(input).style.display='none'
-        document.getElementById(span_id).innerHTML=document.getElementById(input).value
-        document.getElementById(span_id).style.display='block'
+        document.getElementById(span).innerHTML=document.getElementById(input).value
+        document.getElementById(span).style.display='block'
         document.getElementById(edit).innerHTML=
-        `<button class="btn btn-success" id='E-${row}-${col}' onclick=EditRow(${JSON.stringify(data)},${row},${col})>Edit</button>`
+        `<button class="btn btn-success" id='${row}-${col}' onclick=EditRow(${JSON.stringify(data)},${row},${col})>Edit</button>`
                  
     }
 }
 
 
 //function to delete the row 
-function DeleteRow(del_row,data){
-data.splice(del_row)
-document.getElementById(del_row).innerHTML=''
-
+function DeleteRow(delte_row,data){
+  data.splice(delte_row)
+  document.getElementById(delte_row).innerHTML=''
 }
 
 
